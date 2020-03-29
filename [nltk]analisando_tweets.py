fa694c34eecb31027d44d5f588f3a1bd67c71871
Victor Coelho <victoracoelho22@gmail.com>
2020-03-29 01:30:23 -0300

    Update [nltk]analisando_tweets.py

--- [nltk]analisando_tweets.py
+++ [nltk]analisando_tweets.py
@@ -1,23 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-Este é um arquivo de script temporário.
-"""
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from twitterscraper import query_tweets
 from datetime import datetime
 import nltk
-nltk.download('punkt')
-nltk.download()
 from nltk.corpus import stopwords
 import string
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-%matplotlib inline
+import collections
 
 
 atual = datetime.now().date()
@@ -31,11 +22,13 @@ tweets = query_tweets('bolsonaro', begindate=inicio, enddate=atual, limit=limit,
 
 df = pd.DataFrame(t.__dict__ for t in tweets)
 
-data = pd.Series(df['text'])
+data = pd.DataFrame(df['text'])
 
 phrases = list(df['text'])
 
 data.describe()
+
+
 
 def txt_process(txt):
     nopont = [char for char in txt if char not in string.punctuation]
@@ -43,17 +36,35 @@ def txt_process(txt):
     sms = [word for word in nopont.split() if word.lower() not in stopwords.words('portuguese')]
     return sms
 
-teste = data.head(5).apply(txt_process)
+teste = data.apply(txt_process)
 
-proc = CountVectorizer(analyzer=txt_process).fit(data)
+
+counter = collections.Counter(teste[0])
+
+print(counter.most_common(n=50))
+
+lista1 = list(counter.values())
+lista2 = list(counter.keys())
+lista3 = list(counter.most_common())
+
+
+
+plt.figure(figsize=(12,6))
+plt.bar(lista2[21:40], lista1[21:40])
+
+
+
+
+
+### CÓDIGO EM ESTUDO...
+'''proc = CountVectorizer(analyzer=txt_process).fit(data)
 len(proc.vocabulary_)
 
 proc_transform = proc.transform(data)
 proc_transform.shape    
 
 tfidf = TfidfTransformer()
-tfidf = tfidf.fit(proc_transform)
-
+tfidf = tfidf.fit(proc_transform)'''
 
 
 
